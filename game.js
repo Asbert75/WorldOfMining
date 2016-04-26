04111f8cf8e287071d26f0e650af9c9eb0ab4e6d
Erik Olsson <erik_olsson_97@hotmail.com>
2016-04-26 23:23:46 +0200

    Initial git

---
+++ game.js
@@ -0,0 +1,34 @@
+var ores = 0;
+
+function mine(number){
+    ores = ores + number;
+    document.getElementById("ores").innerHTML = ores;
+};
+
+var picks = 0;
+
+function buyBasicPick(){
+    var basicPickCost = Math.floor(10 * Math.pow(1.1,basicPicks));     //works out the cost of this cursor
+    if(ores >= basicPickCost){                                   //checks that the player can afford the cursor
+        basicPicks = basicPicks + 1;                                   //increases number of cursors
+    	ores = ores - basicPickCost;                          //removes the cookies spent
+        document.getElementById('BasicPicks').innerHTML = basicPicks;  //updates the number of cursors for the user
+        document.getElementById('ores').innerHTML = ores;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,basicPicks));       //works out the cost of the next cursor
+    document.getElementById('basicPickCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+
+
+
+window.setInterval(function(){
+	
+	mine(CommonEnchants /* Amount */);
+	
+}, 1000);
+
+
+
+function closeWelcome() {
+    document.getElemenyById("Welcome").hide();
+}
